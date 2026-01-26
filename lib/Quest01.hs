1486363a327eb230121e8515c69dc42844ab78b8
Marco Marinho <marcoobom@gmail.com>
2026-01-26 19:19:20 +0100

    Quest 01

--- lib/Quest01.hs
+++ lib/Quest01.hs
@@ -1,9 +1,20 @@
 module Quest01 where
 
+import Control.Monad.State (MonadState (get), State, evalState, modify)
 import Data.Function ((&))
+import Data.HashMap.Strict qualified as HM
+import Data.IntSet qualified as IS
+import Data.List (mapAccumL)
 import Data.List.Split (splitOn)
 import Debug.Trace (traceShowId)
+import GHC.Arr (Array, Ix, array, bounds, (!))
 import Util (Grid, makeGrid, numCols, numRows, (??))
+
+type MemoKey = (IS.IntSet, Int)
+
+type MemoValue = (Int, Int)
+
+type Memo = HM.HashMap MemoKey MemoValue
 
 play :: Grid Char -> (Int, Int) -> [Char] -> Int
 play grid (r, c) commands = case command of
@@ -36,6 +47,27 @@ bestPlay grid rules = maximum scores
     results = map (\i -> play grid (0, i * 2) rules) slots
     scores = zipWith (\f t -> max ((f * 2) - (t + 1)) 0) results slots
 
+dpMemo :: Array (Int, Int) Int -> IS.IntSet -> Int -> State Memo MemoValue
+dpMemo _ _ 6 = return (0, 0)
+dpMemo scores used idx = do
+  memo <- get
+  case HM.lookup (used, idx) memo of
+    Just val -> return val
+    Nothing -> do
+      let usable = filter (\x -> not (IS.member x used)) [0 .. fst (snd (bounds scores))]
+      results <-
+        mapM
+          ( \i -> do
+              (nextMin, nextMax) <- dpMemo scores (IS.insert i used) (idx + 1)
+              let score = scores ! (i, idx)
+              return (score + nextMin, score + nextMax)
+          )
+          usable
+      let omin = minimum (map fst results)
+          omax = maximum (map snd results)
+      modify (HM.insert (used, idx) (omin, omax))
+      return (omin, omax)
+
 part1 :: String -> String
 part1 input = show resSum
   where
@@ -51,8 +83,16 @@ part2 input = show resSum
     results = map (bestPlay grid) rules
     resSum = sum results
 
+part3 :: String -> String
+part3 input = show result
+  where
+    (grid, rules) = parseInput input
+    associations = [((i, j), max ((play grid (0, i * 2) (rules !! j) * 2) - (i + 1)) 0) | i <- [0 .. (numCols grid `div` 2)], j <- [0 .. length rules - 1]]
+    scores = array ((0, 0), (numCols grid `div` 2, length rules - 1)) associations
+    result = evalState (dpMemo scores IS.empty 0) HM.empty
+
 solve :: String -> Int -> String
 solve input 1 = part1 input
 solve input 2 = part2 input
-solve input 3 = "Part 3 not implemented yet"
+solve input 3 = part3 input
 solve input _ = "Invalid part number"
